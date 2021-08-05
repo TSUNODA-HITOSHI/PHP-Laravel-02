369c94aba78f16ca30555e636e957b8f03c8a023
角田仁志 <shinsui889@hotmail.com>
2021-08-05 00:19:07 +0000

    2021.8.5.9:18

---
+++ practice2.php
@@ -0,0 +1,19 @@
+<?php
+
+$age = 5;
+
+
+if($age >= 10 && $age < 20){
+    echo("teenager");
+}else if($age >= 20 && $age < 30){
+    echo ("twenties");
+}else if($age >= 30 && $age < 40){ 
+    echo("thirties");
+    }else {
+       echo ("other");
+    }
+
+
+
+
+
